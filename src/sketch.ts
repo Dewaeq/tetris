2f72f6e78b7c808c426245a8d2c969046d1f5129
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2022-11-01 12:00:49 +0100

    prevent setup from running twice

--- src/sketch.ts
+++ src/sketch.ts
@@ -6,29 +6,43 @@ export const gridY = 20
 export let gridOffset = 0
 export let cellSize = 0
 export let game: Game
+export let $: p5
 
-export const $ = new p5((_) => { })
+const init = () => {
+    if (window.initialized) return
 
-$.setup = () => {
-    if ($.windowWidth > $.windowHeight) {
-        cellSize = $.windowHeight * 0.75 / gridY
-        gridOffset = cellSize * 3
+    window.initialized = true
+    
+    $ = new p5((_) => { })
+
+    $.setup = () => {
+        if ($.windowWidth > $.windowHeight) {
+            cellSize = $.windowHeight * 0.75 / gridY
+            gridOffset = cellSize * 3
+
+            const canv = $.createCanvas(gridX * cellSize + gridOffset * 2, gridY * cellSize)
+            canv.parent("#canvas")
+        } else {
+            cellSize = $.windowWidth * 0.6 / gridX
+            gridOffset = $.windowWidth * 0.4 / 2
+            const canv = $.createCanvas($.windowWidth, gridY * cellSize)
+            canv.parent("#canvas")
+        }
+
+        game = new Game()
         
-        const canv = $.createCanvas(gridX * cellSize + gridOffset * 2, gridY * cellSize)
-        canv.parent("#canvas")
-    } else {
-        cellSize = $.windowWidth * 0.6 / gridX
-        gridOffset = $.windowWidth * 0.4 / 2
-        const canv = $.createCanvas($.windowWidth, gridY * cellSize)
-        canv.parent("#canvas")
+        $.draw = () => game.draw()
+    
+        $.keyPressed = () => game.input.keyPressed()
+    
+        $.keyReleased = () => game.input.clearIntervals()
     }
-
-
-    game = new Game()
 }
 
-$.draw = () => game.draw()
+init()
 
-$.keyPressed = () => game.input.keyPressed()
-
-$.keyReleased = () => game.input.clearIntervals()
+declare global {
+    interface Window {
+        initialized: boolean
+    }
+}
